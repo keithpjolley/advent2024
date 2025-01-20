f34c7256669917ac118c8ed43743086c3b38875e
Keith P. Jolley <keith.jolley@zainartech.com>
2025-01-20 10:32:10 -0800

    feat: day20 - two stars

--- src/day20.py
+++ src/day20.py
@@ -23,6 +23,9 @@ class Day:
         self._get_raw = GetRawData(
             args, day=os.path.splitext(os.path.basename(__file__))[0]
         )
+        self._savings_threshold = (
+            50 if "input" in args and "test" in args.input else 100
+        )
         self._raw_data = self._get_raw.raw_data.strip()
         self._graph = self._parse_data()
         self._start = [
@@ -56,20 +59,31 @@ class Day:
         if graph is None:
             graph = self._graph
         try:
-            for n in nx.shortest_path(graph, self._start, self._end):
-                graph.nodes[n]["color"] = "g"
+            labels = dict()
+            for i, n in enumerate(
+                nx.shortest_path(graph, self._start, self._end)
+            ):
+                if n not in [self._start, self._end]:
+                    graph.nodes[n]["color"] = "b"
+                labels[n] = i
         except (AttributeError, nx.NetworkXNoPath):
+            labels = {n: n for n in graph.nodes()}
             pass
-        colors = [v["color"] for _, v in graph.nodes(data=True)]
-        positions = {n: (n[0], -n[1]) for n in graph.nodes()}
+        w = max(n[0] for n in graph.nodes()) + 2
+        h = max(n[1] for n in graph.nodes()) + 2
+        for x in range(w):
+            for y in range(h):
+                if (x, y) not in graph.nodes:
+                    graph.add_node((x, y), color="y", node_type="wall")
         nx.draw(
             graph,
-            pos=positions,
-            node_color=colors,
-            with_labels=True,
+            font_size=8,
+            labels=labels,
+            node_color=[v["color"] for _, v in graph.nodes(data=True)],
             node_shape="s",
             node_size=100,
-            font_size=8,
+            pos={n: (n[0], -n[1]) for n in graph.nodes()},
+            with_labels=True,
         )
         if saveto is None:
             plt.show()
@@ -82,9 +96,9 @@ class Day:
         for y, row in enumerate(self._raw_data.split("\n")):
             for x, col in enumerate(row):
                 if col == ".":
-                    graph.add_node((x, y), color="b", node_type="path")
+                    graph.add_node((x, y), color="g", node_type="path")
                 if col == "S":
-                    graph.add_node((x, y), color="g", node_type="start")
+                    graph.add_node((x, y), color="m", node_type="start")
                 if col == "E":
                     graph.add_node((x, y), color="r", node_type="end")
         for node in graph.nodes:
@@ -96,17 +110,24 @@ class Day:
         return graph
 
     def _part1(self: Self) -> int:
+        """! A better way would be to use the shortest path, label each
+        node with its distance from the start and then look for the
+        delta of nodes two straight steps apart.
+        """
         savings = 0
         spl_nocheats = nx.shortest_path_length(
             self._graph, self._start, self._end
         )
+
         for col in range(1, max(n[1] for n in self._graph.nodes) + 1):
             for row in range(1, max(n[0] for n in self._graph.nodes) + 1):
                 if (col, row) not in self._graph.nodes:
                     for j in [(0, 1), (1, 0)]:
-                        if (col + j[0], row + j[1]) not in self._graph.nodes:
-                            continue
-                        if (col - j[0], row - j[1]) not in self._graph.nodes:
+                        if (
+                            (col + j[0], row + j[1]) not in self._graph.nodes
+                        ) or (
+                            (col - j[0], row - j[1]) not in self._graph.nodes
+                        ):
                             continue
                         graph = self._graph.copy()
                         graph.add_node(
@@ -123,8 +144,37 @@ class Day:
                             savings += 1
         return savings
 
+    def _manhattan(
+        self: Self, distance: int, start: tuple[int, int]
+    ) -> list[tuple[int, int]]:
+        """! Return a list of coordinates that are at most `distance`
+        steps away, and not at the starting point."""
+        x, y = start
+        return [
+            (i + x, j + y)
+            for i in range(-distance, distance + 1)
+            for j in range(-distance, distance + 1)
+            if (abs(i) + abs(j)) <= distance and (i, j) != (0, 0)
+        ]
+
     def _part2(self: Self) -> int:
-        return 0
+        shortest_path = nx.shortest_path(self._graph, self._start, self._end)
+        nx.set_node_attributes(self._graph, None, "step")
+        for i, n in enumerate(shortest_path):
+            self._graph.nodes[n]["step"] = i
+        savings = 0
+        for n in shortest_path:
+            for p in self._manhattan(20, n):
+                if p in self._graph.nodes:
+                    s1 = self._graph.nodes[p]["step"]
+                    s2 = self._graph.nodes[n]["step"]
+                    if s1 is not None and s2 is not None:
+                        saving = (
+                            s1 - s2 - (abs(p[0] - n[0]) + abs(p[1] - n[1]))
+                        )
+                        if saving >= self._savings_threshold:
+                            savings += 1
+        return savings
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test_day20.py
+++ tests/test_day20.py
@@ -5,15 +5,10 @@ import src.day20 as day
 
 
 class TestDay(unittest.TestCase):
-    test_input = ["--input", "data/day20_test0.txt"]
-    test_data = []
-
-    def test_parse_args(self: Self) -> None:
-        test = day.Day(self.test_input)
-        self.assertEqual(test._data, self.test_data)
+    test_input = ["--input", "data/day20_test1.txt"]
 
     def test_parts(self: Self) -> None:
-        tests = [(0, 0, self.test_input)]
+        tests = [(3, 1582, self.test_input)]
         # No use using up github action minutes for this.
         # tests.append((0, 0, []))
         for p1, p2, test in tests:
